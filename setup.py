58654d18744bd9e62430df0f0c34d81ec05d49f8
christopher-christofi <christopherlchristofi@outlook.com>
2021-07-15 15:52:15 +0100

    Add logging, update setup, add help documentation

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='tabula-py-cli-wrapper',
     version='1.0.0',
-    py_modules=['tabulapycliwrapper'],
+    py_modules=['tabulapycliwrapper', 'log'],
     install_requires=[
         'Click',
         'tabula-py',
     ],
     entry_points={
         'console_scripts': [
-            'tabulapycliwrapper = tabulapycliwrapper:cli',
+            'tabulapycli = tabulapycliwrapper:cli',
         ],
     },
 
--- tabulapycliwrapper.py
+++ tabulapycliwrapper.py
@@ -1,28 +1,53 @@
-import click, tabula
+import click, tabula, logging
+from log import logger
 
-
+@click.option('--pdf-data',
+        type=click.Path(()),
+        help='Provide the filename and filepath to the PDF data source.'
+)
 @click.option(
-        '--pdf-data',
-        type=click.Path(())
-        )
-@click.option(
-        '--ouput-path',
+        '--output-path',
         default='output.csv',
-        type=click.Path(())
-        )
+        type=click.Path(()),
+        help='Provide a filepath for the csv output. Default is output.csv'
+)
 @click.option(
         '--output-format',
         default='csv',
-        type=click.Choice(['csv','JSON','tsv'])
-        )
+        type=click.Choice(['csv','JSON','tsv']),
+        help='Select an applicable output format. Default is csv'
+)
 @click.option(
         '--pages',
-        type=click.INT
-        )
+        type=click.INT,
+        help='Provide a page number that holds your selected table. Default is all pages.'
+)
+@click.option(
+        '--log',
+        default='tabulapycliwrapper.log',
+        type=click.Path(()),
+        help='Provide a filepath for a logging file. Default is tabulapycliwrapper.log'
+)
 @click.command()
-def cli(pdf_data, output_path, output_format, pages):
+def cli(pdf_data, output_path, output_format, pages, log):
 
-    if not pages: pages = 'all'
+    '''Responsible for initiating tabula-py functionality; the conversion of PDF data tables to another format, such as: csv'''
 
-    tabula.convert_into(pdf_data, output_path, output_format, pages)
 
+    logger(log)
+
+    if not pages:
+        pages = 'all'
+
+    logging.info('converting file: {input}. Page number: {page}'.format(
+            input=pdf_data,
+            page=pages
+            ))
+
+    tabula.convert_into(pdf_data, output_path, output_format=output_format, pages=str(pages))
+
+    logging.info('completed converting file: {input} to {output}. Page number: {page}'.format(
+            input=pdf_data,
+            output=output_path,
+            page=pages
+            ))
